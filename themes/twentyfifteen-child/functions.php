e62549d9c88eb13cf058979d679e1cf913525570
Kent <fobster0@gmail.com>
2015-06-28 08:36:05 -0700

    add swipe events to attachments via hammer js

--- themes/twentyfifteen-child/functions.php
+++ themes/twentyfifteen-child/functions.php
@@ -1,7 +1,23 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
+add_action( 'wp_enqueue_scripts', 'theme_name_scripts' );
+
+function theme_name_scripts() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
+    wp_enqueue_script(
+		'hammerjs',
+		get_stylesheet_directory_uri() . '/js/hammer.min.js',
+		array( 'jquery' )
+	);
+
+	wp_register_script(
+        'main',
+        get_stylesheet_directory_uri() . '/js/main.js',
+        false,
+        '1.0',
+        true
+    );
+
+    wp_enqueue_script( 'main' );
 }
 ?>
---
+++ themes/twentyfifteen-child/js/main.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function($j) {
+    // Inside of this function, $() will work as an alias for jQuery()
+    // and other libraries also using $ will not be accessible under this shortcut
+
+var attachmentEl = document.getElementsByClassName('entry-attachment')[0];
+
+    var hammer = new Hammer(attachmentEl);
+
+    hammer.get('pan').set({enable: false});
+    hammer.get('swipe').set({velocity: .1});
+	var nextLink = $j('article.attachment .nav-next a');
+	nextLink.bind('click',function(e){
+		// alert('next');
+		window.location = e.target.href;
+	});
+
+	var prevLink = $j('article.attachment .nav-previous a');
+	prevLink.bind('click',function(e){
+		// alert('prev');
+		window.location = e.target.href;
+	});
+
+    hammer.on('swipeleft', function(evt){
+    	nextLink.trigger('click');
+    });
+
+    hammer.on('swiperight', function(evt){
+    	prevLink.trigger('click');
+    });
+});
